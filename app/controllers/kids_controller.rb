ed36f2aa31526efacc0b8f2d250180ca1a972fef
saworieza <saworieza@gmail.com>
2015-01-15 16:51:21 +0300

    created pages, added devise, paperclip, and created images for projects

---
+++ app/controllers/kids_controller.rb
@@ -0,0 +1,47 @@
+class KidsController < ApplicationController
+  before_action :set_kid, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @kids = Kid.all
+    respond_with(@kids)
+  end
+
+  def show
+    respond_with(@kid)
+  end
+
+  def new
+    @kid = Kid.new
+    respond_with(@kid)
+  end
+
+  def edit
+  end
+
+  def create
+    @kid = Kid.new(kid_params)
+    @kid.save
+    respond_with(@kid)
+  end
+
+  def update
+    @kid.update(kid_params)
+    respond_with(@kid)
+  end
+
+  def destroy
+    @kid.destroy
+    respond_with(@kid)
+  end
+
+  private
+    def set_kid
+      @kid = Kid.find(params[:id])
+    end
+
+    def kid_params
+      params[:kid]
+    end
+end
---
+++ app/views/kids/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @kid, :id, :created_at, :updated_at
