85abfcbe312a9938f0974e57c1b9304d2f1c6e05
saworieza <saworieza@gmail.com>
2015-01-16 12:10:48 +0300

    added jquery light box css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.precompile += %w( bootstrap-responsive.css )
 Rails.application.config.assets.precompile += %w( prettyPhoto.css )
 Rails.application.config.assets.precompile += %w( flexslider.css )
 Rails.application.config.assets.precompile += %w( custom-styles.css )
+Rails.application.config.assets.precompile += %w( jquery.lightbox-0.5.css )
 
 Rails.application.config.assets.precompile += %w( jquery-1.8.3.min.js )
 Rails.application.config.assets.precompile += %w( bootstrap.js )
