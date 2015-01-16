1359155d3e438dac7fe6307b8956cc920a369d49
saworieza <saworieza@gmail.com>
2015-01-16 11:22:47 +0300

    added home template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,14 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( bootstrap-responsive.css )
+Rails.application.config.assets.precompile += %w( prettyPhoto.css )
+Rails.application.config.assets.precompile += %w( flexslider.css )
+Rails.application.config.assets.precompile += %w( custom-styles.css )
+
+Rails.application.config.assets.precompile += %w( jquery-1.8.3.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( jquery.prettyPhoto.js )
+Rails.application.config.assets.precompile += %w( jquery.flexslider.js )
+Rails.application.config.assets.precompile += %w( jquery.custom.js )
